8b6fda0139d9abc3b6c34427cffd7c890b834bdd
Daniela Carrasco <dalbacarrasco@gmail.com>
2026-02-12 16:59:14 -0400

    update: correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "hhtp://dalbacarrasco.github.io/to-do-list-react",
+  base: "https://dalbacarrasco.github.io/to-do-list-react",
   plugins: [react()],
 })
