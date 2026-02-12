00c751f1dedeb272713ad77f59cbee4ce3abcd26
Daniela Carrasco <dalbacarrasco@gmail.com>
2026-02-12 16:45:28 -0400

    add: github actions and deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "hhtp://dalbacarrasco.github.io/to-do-list-react",
   plugins: [react()],
 })
